f62a3c314f342b5f40a4259f39f52921c9dbe899
10287fio <10287fio@gmail.com>
2024-09-24 22:36:47 +0900

    .

--- publish-controller/src/main/java/fragranceia/publish/controller/PublishControllerApplication.java
+++ publish-controller/src/main/java/fragranceia/publish/controller/PublishControllerApplication.java
@@ -1,6 +1,5 @@
 package fragranceia.publish.controller;
 
-import lombok.Builder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
