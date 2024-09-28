e94c3f8d022c2711d3fc125e4de07360e22bbeda
10287fio <10287fio@gmail.com>
2024-09-28 17:54:14 +0900

    .

--- manuscript-repository/build.gradle.kts
+++ manuscript-repository/build.gradle.kts
@@ -6,4 +6,6 @@ group = "fragranceia.manuscript.repository"
 version = "0.0.1"
 
 dependencies {
+    implementation("org.postgresql:postgresql")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 }
